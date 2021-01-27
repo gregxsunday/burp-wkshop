02d0d5ed586c90a1dfc2fb64c4fe0bbc4fe0b142
gregxsunday <gregxsunday@gmail.com>
2021-01-27 20:55:43 +0100

    added history task

--- app/core/views.py
+++ app/core/views.py
@@ -4,6 +4,18 @@ from app.core.repository import *
 
 mod = Blueprint('core', __name__)
 
+flags = {
+  'history': r'msfi{123}'
+}
+
 @mod.route('/')
 def index():
   return (render_template('core/index.html', nav='setup'))
+
+@mod.route('/history2')
+def history2():
+  return f'<html>{flags["history"]}</html>'
+
+@mod.route('/history')
+def history():
+  return (render_template('core/history.html', nav='history'))
