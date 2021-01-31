6f8a3b10e180f3325311a4803b18299f92b128f1
gregxsunday <gregxsunday@gmail.com>
2021-01-31 18:12:35 +0100

    added repeater task

--- app/core/views.py
+++ app/core/views.py
@@ -6,7 +6,8 @@ mod = Blueprint('core', __name__)
 
 flags = {
   'history': r'msfi{123}',
-  'intercept': r'msfi{456}'
+  'intercept': r'msfi{456}',
+  'repeater': r'msfi{789}'
 }
 
 @mod.route('/')
@@ -30,3 +31,25 @@ def intercept():
       return f'<html>no flag for you</html>'
   else:
     return (render_template('core/intercept.html', nav='intercept'))
+
+@mod.route('/repeater')
+def repeater():
+  return (render_template('core/repeater.html', nav='repeater'))
+
+@mod.route('/repeater.json', methods=['GET', 'POST', 'PUT'])
+def repeater_json():
+  if request.method == 'PUT':
+    if request.is_json:
+      req = request.get_json()
+      try:
+        key = req['key']
+        if key == 5:
+          return jsonify({'flag': flags['repeater']})
+        else:
+          return jsonify({'error':'this is not the correct key my friend'})
+      except KeyError:
+        return jsonify({'error':'I want the key in the JSON my friend'}), 400
+    else:
+      return jsonify({'error':'I want JSON my friend'}), 400
+  else:
+    return jsonify({'error':'Bad method my friend'}), 405
