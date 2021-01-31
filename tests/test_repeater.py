b1ca3456e79a6116e48ae850a58ee4e066a16938
gregxsunday <gregxsunday@gmail.com>
2021-01-31 18:12:47 +0100

    added tests to repeater

---
+++ tests/test_repeater.py
@@ -0,0 +1,44 @@
+import pytest
+
+from app import app
+import json
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_bad_method(client):
+    resp = client.get('/repeater.json')
+    assert resp.status_code == 405
+    resp = client.post('/repeater.json')
+    assert resp.status_code == 405
+
+def test_bad_form(client):
+    d = {'key': 0}
+    resp = client.put('/repeater.json', data=json.dumps(d))
+    assert resp.status_code == 400
+
+def test_bad_key(client):
+    d = {'key': 1}
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    resp = client.put('/repeater.json', data=json.dumps(d), headers=headers)
+    assert b'not the correct key' in resp.data 
+
+def test_json_without_key(client):
+    d = {'ke': 1}
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    resp = client.put('/repeater.json', data=json.dumps(d), headers=headers)
+    assert b'I want the key' in resp.data 
+
+def test_valid_request(client):
+    d = {'key': 5}
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    resp = client.put('/repeater.json', data=json.dumps(d), headers=headers)
+    assert b'msfi{' in resp.data 
