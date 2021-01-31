f3b1498cb679232eebe2a3e657eb0bef5d96a2a2
gregxsunday <gregxsunday@gmail.com>
2021-01-31 18:13:30 +0100

    added tests to repeater task

--- tests/test_repeater.py
+++ tests/test_repeater.py
@@ -11,13 +11,17 @@ def client():
 def test_bad_method(client):
     resp = client.get('/repeater.json')
     assert resp.status_code == 405
+    assert b'msfi{' not in resp.data 
     resp = client.post('/repeater.json')
     assert resp.status_code == 405
+    assert b'msfi{' not in resp.data 
+
 
 def test_bad_form(client):
     d = {'key': 0}
     resp = client.put('/repeater.json', data=json.dumps(d))
     assert resp.status_code == 400
+    assert b'msfi{' not in resp.data 
 
 def test_bad_key(client):
     d = {'key': 1}
@@ -26,6 +30,7 @@ def test_bad_key(client):
     }
     resp = client.put('/repeater.json', data=json.dumps(d), headers=headers)
     assert b'not the correct key' in resp.data 
+    assert b'msfi{' not in resp.data 
 
 def test_json_without_key(client):
     d = {'ke': 1}
@@ -34,6 +39,7 @@ def test_json_without_key(client):
     }
     resp = client.put('/repeater.json', data=json.dumps(d), headers=headers)
     assert b'I want the key' in resp.data 
+    assert b'msfi{' not in resp.data 
 
 def test_valid_request(client):
     d = {'key': 5}
